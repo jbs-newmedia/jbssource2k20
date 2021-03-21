78089721cd3b66f156f8d00741bb392f4b467993
Juergen Schwind <info@jbs-newmedia.de>
2021-03-21 15:50:28 +0100

    Package entfernt

--- source/jbsproject.jbsnmsite/stable/modules/jbsnm/tpl/content.tpl.php
+++
@@ -1,3 +0,0 @@
-<h3 class="text-primary d-block text-center">Willkommen bei der JBS New Media GmbH</h3>
-<hr class="pt-2"/>
-<img src="<?php echo JBSNewMedia\Site\Core::getResourcePath();?>img/jbs-logo-symbol.svg" style="max-height: 420px;" title="JBS New Media GmbH" alt="jbs new media gmbh"/><br/>
