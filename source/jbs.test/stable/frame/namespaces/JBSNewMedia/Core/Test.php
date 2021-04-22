31b921593167fb334ae208a9424b70326e3210d6
Juergen Schwind <info@jbs-newmedia.de>
2021-04-22 17:08:40 +0200

    Test-Package

---
+++ source/jbs.test/stable/frame/namespaces/JBSNewMedia/Core/Test.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the Core package
+ *
+ * @author Juergen Schwind
+ * @copyright Copyright (c) JBS New Media GmbH - Juergen Schwind (https://jbs-newmedia.com)
+ * @package Core
+ * @link https://jbs-newmedia.com
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3
+ */
+
+namespace JBSNewMedia\Core;
+
+use osWFrame\Core as osWFrame;
+
+class Test {
+
+	use osWFrame\BaseStaticTrait;
+
+	/**
+	 * Major-Version der Klasse.
+	 */
+	private const CLASS_MAJOR_VERSION=1;
+
+	/**
+	 * Minor-Version der Klasse.
+	 */
+	private const CLASS_MINOR_VERSION=0;
+
+	/**
+	 * Release-Version der Klasse.
+	 */
+	private const CLASS_RELEASE_VERSION=0;
+
+	/**
+	 * Extra-Version der Klasse.
+	 * Zum Beispiel alpha, beta, rc1, rc2 ...
+	 */
+	private const CLASS_EXTRA_VERSION='';
+
+
+}
+
+?>
